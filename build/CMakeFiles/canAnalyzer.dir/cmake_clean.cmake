f2759c48daaf67bd908e1d86a2f972ed21c765d4
spatel <swapnil.patel@horstmansystems.com>
2022-06-22 15:03:06 -0400

    first commit

---
+++ build/CMakeFiles/canAnalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/canAnalyzer.dir/main.cpp.obj"
+  "CMakeFiles/canAnalyzer.dir/main.cpp.obj.d"
+  "canAnalyzer.exe"
+  "canAnalyzer.exe.manifest"
+  "canAnalyzer.pdb"
+  "libcanAnalyzer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/canAnalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ canlib/README.md
@@ -0,0 +1,4 @@
+# Kvaser CANLIB SDK
+
+- CANLIB V5.39
+- https://www.kvaser.com/developer/canlib-sdk/
